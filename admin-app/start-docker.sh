8bdb69463dae6a538cf80ed787e6b27ef8cf72d6
Roryg <giannichan@gmail.com>
2017-10-19 17:22:36 +0100

    move sql backup and adds start script

--- admin-app/start-docker.sh
+++ admin-app/start-docker.sh
@@ -1,26 +1,32 @@
-Build the docker image from scratch:
+#!/bin/bash
+
+echo "Removing existing containers"
+
+docker stop admin-mysql
+docker stop admin-app
+
+docker rm admin-mysql
+docker rm admin-app
+
+echo "Rebuilding image"
 
 docker build -t eca/admin-app-image .
-
-Build and run latest mysql container:
 
 docker run -d --name admin-mysql -e MYSQL_ROOT_PASSWORD=change_me_too \
  -e MYSQL_DATABASE=wwwlearn_school \
  -e MYSQL_USER=wwwlearningenerg \
  -e MYSQL_PASSWORD=change_me mysql:latest
 
+echo "Runnning app container"
+
 docker run -d -p 80:80 --name admin-app \
   -e "DATABASE_URL=mysql://root:change_me_too@admin-mysql/wwwlearn_school?encoding=utf8&timezone=UTC&cacheMetadata=true" \
   -e "SESSION_DEFAULTS=database" --link admin-mysql:mysql eca/admin-app-image
 
-To attach bash: 
+echo "Waiting on container to be ready."
+sleep 20s
 
-docker exec -it <container name> bash
-
-To run an .sql import on db container:
-
+echo "Creating tables from .sql file"
 docker exec -i admin-mysql mysql -uwwwlearningenerg -pchange_me wwwlearn_school < original-db-dump.sql
 
-To stop containers: 
-
-docker stop <container name>
+echo "Done."
